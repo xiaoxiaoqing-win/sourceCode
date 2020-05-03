03469d8fc32c1a99c54c922407f4fc208f8e111f
Alberqing <838248029@qq.com>
2020-05-03 10:42:09 +0800

    bin dbind

---
+++ oop/bind.js
@@ -0,0 +1,14 @@
+// bind方法是预先改变this指向，不立即执行函数
+
+~function anymous(proto) {
+    function bind(context = window, ...args) {
+        context === null ? context = window : null;
+        var _this = this;
+        return function() {
+            let innerArgs = Array.prototype.slice.call(0);
+            let finalArgs = innerArgs.concat(args);
+            return _this.call(context, ...finalArgs);
+        }
+    }
+    proto.bind = bind;
+}(Function.prototype)
