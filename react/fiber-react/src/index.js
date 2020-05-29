0e977f9c21a0bc12640a3de63a5bf87ee2abac79
Alberqing <838248029@qq.com>
2020-05-29 15:05:56 +0800

    从零实现react搭建

---
+++ react/fiber-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let element = ()
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
