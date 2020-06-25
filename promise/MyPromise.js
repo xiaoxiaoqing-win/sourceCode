79816276d7198faba1080f38e7b6cb60ae32f4af
zhengyunqing <zhengyunqing@kuaishou.com>
2020-06-25 22:05:02 +0800

    排查promise原因，还未找到

--- promise/MyPromise.js
+++ promise/MyPromise.js
@@ -8,10 +8,13 @@ class MyPromise {
 
         // 定义change方法，因为我们发现好像resolve和reject方法共同的地方还挺多🤔
         let change = (status, value) => {
+            // console.log(value, 999);
             if (this.status !== "pending") return;  // 状态一旦改变，就不会再变
+
             this.status = status;
             this.value = value;
 
+            console.log(this.value, "111", this.status);
             // 根据状态判断要执行成功的方法或失败的方法
             let fnArr = status === "resolved" ? this.resolveArr : this.rejectArr;
 
@@ -88,6 +91,7 @@ class MyPromise {
             })
 
             this.rejectArr.push(reason => {
+                debugger
                 try {
                     let x = rejectFn(reason);
 
